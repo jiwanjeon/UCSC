111205e7fca94a2bbd6b36b3d7281b159d6ebec1
jjeon17 <jjeon17@ucsc.edu>
2021-02-08 02:47:38 +0900

    push

---
+++ asgn4/bm.h
@@ -0,0 +1,28 @@
+#ifndef __BITMATRIX_H__
+#define __BITMATRIX_H__
+
+#include <stdint.h>
+
+ typedef struct  BitMatrix{
+    uint32_t rows ;
+    uint32_t cols ;
+    uint8_t ** mat;
+}BitMatrix;
+
+BitMatrix *bm_create(uint32_t rows, uint32_t cols);
+
+void bm_delete(BitMatrix **m);
+
+uint32_t bm_rows(BitMatrix *m);
+
+uint32_t bm_cols(BitMatrix *m);
+
+void bm_set_bit(BitMatrix *m, uint32_t r, uint32_t c);
+
+void bm_clr_bit(BitMatrix *m, uint32_t r, uint32_t c);
+
+uint8_t bm_get_bit(BitMatrix *m, uint32_t r, uint32_t c);
+
+void bm_print(BitMatrix *m);
+
+#endif
---
+++ asgn4/decoder.c
@@ -0,0 +1,42 @@
+#include "bm.h"
+#include "hamming.h"
+#include <stdio.h>
+#include <unistd.h>
+
+#define OPTIONS "i:o:"
+/**
+ * Main method of the Decode
+ */
+int main(int argc, char **argv) {
+  int opt = 0;
+  // Create the parity
+  BitMatrix *parity;
+  parity = bm_create(8, 4);
+  char *inputFileName = NULL;
+  char *outputFileName = NULL;
+
+  // Parse the command line option
+  while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+    switch (opt) {
+    case 'i':
+      inputFileName = optarg;
+      break;
+    case 'o':
+      outputFileName = optarg;
+      break;
+    default:
+      printf("%s\n", "Invalid option");
+    }
+  }
+  if (NULL == inputFileName) {
+    printf("%s\n", "Input file path is required");
+  } else {
+    printf("%s\n", inputFileName);
+  }
+  if (NULL == outputFileName) {
+    printf("%s\n", "Output file path is required");
+  } else {
+    printf("%s\n", outputFileName);
+  }
+  return 0;
+}
